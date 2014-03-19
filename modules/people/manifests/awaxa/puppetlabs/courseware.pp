afc5654020af254a319bd5c0155816720508d3b6
Greg Kitson <greg.kitson@gmail.com>
2014-03-19 14:29:51 -0700

    repo deleted

--- modules/people/manifests/awaxa/puppetlabs/courseware.pp
+++ modules/people/manifests/awaxa/puppetlabs/courseware.pp
@@ -1,7 +1,5 @@
 class people::awaxa::puppetlabs::courseware {
 
-  people::awaxa::puppetlabs::repo { 'awaxa/courseware-fundamentals': }
-
   people::awaxa::globalgem { 'showoff': }
 
   include imagemagick
