a5183342779a0776ad8de0b32f669ee1600bd4e0
Greg Kitson <greg.kitson@gmail.com>
2014-08-29 08:25:16 -0700

    anchor srcdir class parameter to top scope in people::awaxa::puppet

--- modules/people/manifests/awaxa/puppet.pp
+++ modules/people/manifests/awaxa/puppet.pp
@@ -1,5 +1,5 @@
 class people::awaxa::puppet {
-  repository { "${boxen::config::srcdir}/puppet": source => 'puppetlabs/puppet' }
-  repository { "${boxen::config::srcdir}/facter": source => 'puppetlabs/facter' }
-  repository { "${boxen::config::srcdir}/hiera":  source => 'puppetlabs/hiera' }
+  repository { "${::boxen::config::srcdir}/puppet": source => 'puppetlabs/puppet' }
+  repository { "${::boxen::config::srcdir}/facter": source => 'puppetlabs/facter' }
+  repository { "${::boxen::config::srcdir}/hiera":  source => 'puppetlabs/hiera' }
 }
