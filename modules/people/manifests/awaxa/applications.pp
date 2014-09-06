7f200bebb4fa60009ea876f9416179ac4111c1ad
Greg Kitson <greg.kitson@gmail.com>
2014-09-05 18:46:09 -0700

    add fixed awaxa/puppet-powerline
    This reverts commit b13c2c10dda18228fd4eeffa12569d38d41a1212.

--- modules/people/manifests/awaxa/applications.pp
+++ modules/people/manifests/awaxa/applications.pp
@@ -12,6 +12,7 @@ class people::awaxa::applications {
   include java
   include nimbus
   include onepassword
+  include powerline
   include prince
   include python
   include python::virtualenvwrapper
