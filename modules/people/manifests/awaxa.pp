99b803944d68230fed9e64b83959c1a3cb6fbb0a
Greg Kitson <greg.kitson@gmail.com>
2014-09-05 20:10:04 -0700

    remove remaining trace of unnecessary pattern

--- modules/people/manifests/awaxa.pp
+++ modules/people/manifests/awaxa.pp
@@ -1,7 +1,7 @@
 class people::awaxa {
-  include '::people::awaxa::applications'
-  include '::people::awaxa::dotfiles'
-  include '::people::awaxa::preferences'
-  include '::people::awaxa::puppet'
-  include '::people::awaxa::vagrant'
+  include people::awaxa::applications
+  include people::awaxa::dotfiles
+  include people::awaxa::preferences
+  include people::awaxa::puppet
+  include people::awaxa::vagrant
 }
