00798e04a30ae126c51e29ba729c0f66d5122c0b
Greg Kitson <greg.kitson@gmail.com>
2014-10-18 22:07:38 -0700

    remove vmware_fusion
    need a version 7 license for yosemite

--- modules/people/manifests/awaxa/vagrant.pp
+++ modules/people/manifests/awaxa/vagrant.pp
@@ -3,12 +3,10 @@ class people::awaxa::vagrant {
     version     => '4.3.18',
     patch_level => '96516',
   }
-  include vmware_fusion
   class { '::vagrant': version => '1.6.5' }
 
   people::awaxa::linked_dotfile { '.vagrant.d/Vagrantfile': }
 
-  #vagrant::plugin { 'vagrant-vmware-fusion': }
   vagrant::plugin { 'vagrant-multiprovider-snap': }
   vagrant::plugin { 'vagrant-cachier': }
   #vagrant::plugin { 'oscar': }
