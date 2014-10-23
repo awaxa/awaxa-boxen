75decc61f4fd69fb1c189a3f6227efb0ed1b98a7
Greg Kitson <greg.kitson@gmail.com>
2014-10-22 17:14:28 -0700

    use homebrew-cask

--- modules/people/manifests/awaxa/applications.pp
+++ modules/people/manifests/awaxa/applications.pp
@@ -1,33 +1,36 @@
 class people::awaxa::applications {
 
-  include chrome
-  include dropbox
+  include brewcask
+
+  package { [
+    'dropbox',
+    'gimp',
+    'google-chrome',
+    'google-hangouts',
+    'hipchat',
+    'mailbox',
+    'nimbus',
+    'onepassword',
+    'sizeup',
+    'tunnelblick',
+    'vlc',
+  ]:
+    ensure   => present,
+    provider => 'brewcask',
+  }
+
   include foreman
-  include gimp
-  include googledrive
   include heroku
-  include hipchat
   include iterm2::colors::arthur
   include iterm2::colors::saturn
   include iterm2::colors::solarized_dark
   include iterm2::colors::solarized_light
   include iterm2::colors::zenburn
   include iterm2::stable
-  class { 'mailbox': version => '0.3.14' }
-  include nimbus
-  include onepassword
   include prince
-  include sizeup
-  include tunnelblick::beta
-  include vlc
 
   class { 'powerline':
     manage_font_library_dir => true,
   }
 
-  package { 'GoogleVoiceAndVideoSetup':
-    ensure   => present,
-    source   => 'http://dl.google.com/googletalk/googletalkplugin/GoogleVoiceAndVideoSetup.dmg',
-    provider => 'pkgdmg',
-  }
 }
