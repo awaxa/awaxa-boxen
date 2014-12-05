069cfaa4e1eb2d6d747b9e4fdf08ff284b1cedb3
Greg Kitson <greg.kitson@gmail.com>
2014-12-04 21:14:54 -0800

    hardcode node version in people::awaxa::nodejs

--- modules/people/manifests/awaxa/nodejs.pp
+++ modules/people/manifests/awaxa/nodejs.pp
@@ -1,9 +1,8 @@
 class people::awaxa::nodejs {
-  include nodejs::global
   nodejs::module { [
     'hubot',
     'keybase-installer',
   ]:
-    node_version => $nodejs::global::version,
+    node_version => 'v0.10',
   }
 }
